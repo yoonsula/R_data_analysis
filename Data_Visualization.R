8337b2ed6110aed1dbc3fe808835091461f655e6
yoonsula <78685157+yoonsula@users.noreply.github.com>
2021-02-25 15:52:53 +0900

    Data_Visualization_fourth

--- Data_Visualization.R
+++ Data_Visualization.R
@@ -133,3 +133,84 @@ pie3D(T_sales,main="주간 매출 변동", col=rainbow(length(T_sales)), cex=0.8,  lab
 #박스그래프 그리기
 boxplot(abc,def,ghi)
 boxplot(abc,def,ghi,col=c("yellow","cyan","green"),names=c("baseball","soccerball","beachball"),horizontal=TRUE)
+
+#그래프 그리기의 부가적인 기능
+plot(1:10)
+par(new=T)
+plot(10:1)
+plot.new()
+plot(1:100)
+
+plot.new()
+plot(-4:4,-4:4,type='n')
+#type="n" =>좌표를 찍지 않음 
+points(rnorm(200),rnorm(200),pch="+",col="red")
+par(new=T)
+points(rnorm(200),rnorm(200),pch="o",col="cyan")
+#rnorm:난수발생, dnorm:확률밀도함수, pnorm:누적분포함수, qnorm:분위수
+
+#꺾은선 그래프
+x<-c(1:10)
+y<-c(x*x)
+y
+plot(x,y,type='n',main="Title")
+for(i in 1:5) lines(x,(y+i*5),col=i,lty=i)
+
+#선분, 화살표, 사각형, 문자열, 직선 그리기
+x<-c(1,3,6,8,9)
+y<-c(12,56,78,32,9)
+plot(x,y)
+segments(6,78,8,32)  #점들을 연결
+arrows(3,56,1,12)    #화살표
+rect(4,20,6,30,density=10) #사각형 그리기, 밀도는 3
+
+text(4,40,"sample",srt=55)  #각도 55
+mtext("상단의 문자열입니다",side=3) #mtext:그래프 위에 글자 출력
+mtext("우측의 문자열입니다",side=4)
+box(lty=2,col="red")
+axis(1,pos=40,at=0:10,col=2)
+axis(2,pos=5,at=10:60,col=3)
+
+#연습문제
+x<-c(1:10)
+y<-exp(x)
+plot(x,y,type='n',main="Title")
+for(i in 1:10) lines(x,(y+i*5),col=i,lty=i)
+y<-x*x
+plot(x,y,type='n',main="Title")
+for(i in 1:10) lines(x,(y+i*5),col=i,lty=i)
+
+mtext("Right Side Text", side=4,adj=0.5)  
+#adj:텍스트의 위치(0:왼쪽 또는아래, 1:오른쪽 또는 위, 0.5:가운데)
+abline(1,2) #직선을 추가함. a:절편, b:기울기
+box(lty=2,col="red")
+axis(1,pos=50, at=0:10, col=2)
+axis(2,pos=6, at=0:100,col=3)
+
+#두 종류 그래프 조합하기
+x<-c(1,2,1,4,5,4,5,2,3,5,2,6,7,3,7,8,6,5,4,7,7,6,5,7,8,9,8)
+par(mfrow=c(1,2))
+hist(x)
+hist(x,probability=T, main="Histogram with density line")
+lines(density(x))
+
+#그래프 종류 소개
+#Sunflowerplot 그래프
+zz<-iris[,1:2]
+sunflowerplot(zz)
+
+#Stars 그래프
+data(mtcars)
+head(mtcars)
+stars(mtcars[,1:4])
+
+#Persp,Contour 그래프
+x1<-seq(-3,3,length=50)
+x2<-seq(-4,4,length=60)
+f<-function(x1,x2){
+  x1^2+x2^2+x1*x2
+}
+y = outer(x1,x2,FUN=f)
+persp(x1,x2,y)
+contour(x1,x2,y)
+image(x1,x2,y)
